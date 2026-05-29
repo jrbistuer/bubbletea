a41d6f2a3fa81ae5f6bcc8990f0ba637cccef55e
jrbistuer <jrbistuer@gmail.com>
2026-05-29 13:21:49 +0200

    CORS 2

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost", "http://127.0.0.1"],
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
